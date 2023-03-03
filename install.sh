de674a3a7b9f6cd56cd7966a6ab6858dcebaf765
Matt Miller <mattmillerf@pm.me>
2023-03-03 17:55:38 -0500

    add formatting

--- install.sh
+++ install.sh
@@ -1,38 +1,58 @@
 #!/usr/bin/env bash
 
-BLUE="34"
-YELLOW="33"
-MAGENTA="35"
-RED="31"
-BOLDBLUE="\e[1;${BLUE}m"
-BOLDYELLOW="\e[1;${YELLOW}m"
-BOLDMAGENTA="\e[1;${MAGENTA}m"
-BOLDRED="\e[1;${RED}m"
-ENDCOLOR="\e[0m"
+grey="\\e[37m"
+blue="\\e[36m"
+red="\\e[31m"
+green="\\e[32m"
+reset="\\e[0m"
 
 GITHUB_USER=thesandybridge
 GITHUB_REPO=$1
 BINARY=$2
 LOCAL_PATH=~/.local/bin
 
+cat << "EOM" 
+┃┃┃┃┃┃┃┃┃┃┃┃┏┓┃┃┃┃┓┃┃┃┃┃┃┏┓┃┃┃┃┃┃
+┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃
+┏━━┓━━┓┃━┓┃━┛┃┓┃┏┓┗━┓━┓┓━┛┃━━┓━━┓
+┃━━┫┃┓┃┃┏┓┓┏┓┃┃┃┃┃┏┓┃┏┛┫┏┓┃┏┓┃┏┓┃
+┣━━┃┗┛┗┓┃┃┃┗┛┃┗━┛┃┗┛┃┃┃┃┗┛┃┗┛┃┃━┫
+┗━━┛━━━┛┛┗┛━━┛━┓┏┛━━┛┛┃┛━━┛━┓┃━━┛
+┃┃┃┃┃┃┃┃┃┃┃┃┃┃━┛┃┃┃┃┃┃┃┃┃┃┃━┛┃┃┃┃
+┃┃┃┃┃┃┃┃┃┃┃┃┃┃━━┛┃┃┃┃┃┃┃┃┃┃━━┛┃┃┃
+    Custom Binary Installer
+EOM
+
+# Logging, loosely based on http://www.ludovicocaldara.net/dba/bash-tips-4-use-logging-levels/
+info() { echo -e "${blue}[+] $*${reset}"; }
+error() { echo -e "${red}[E] $*${reset}"; }
+debug() { if [[ "${DEBUG}" == "true" ]]; then echo -e "${grey}[D] $*${reset}"; fi }
+success() { echo -e "${green}[✔] $*${reset}"; }
+fail() { echo -e "${red}[✖] $*${reset}"; }
+
 if [[ -z $1 ]]; then
-    echo -e "${BOLDRED}[E]${ENDCOLOR} No repository provided"
+    fail "No repository provided"
     exit 1
 fi
 
 
 if [[ -z $2 ]]; then
-    echo -e "${BOLDRED}[E]${ENDCOLOR} No binary name provided"
+    fail "No binary name provided"
     exit 1
 fi
 
-echo -e "${BOLDBLUE}[+]${ENDCOLOR} Downloading file from github.com/${GITHUB_USER}/${GITHUB_REPO}..."
-
+info "Fetching binary from github.com/${GITHUB_USER}/${GITHUB_REPO}..."
 download_linux=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest \
 | grep "browser_download_url.*amd64"  \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | xargs)
 
-wget $download_linux -O $LOCAL_PATH/$BINARY && chmod +x $LOCAL_PATH/$BINARY
+info "Downloading binary..."
+wget -nv $download_linux -O $LOCAL_PATH/$BINARY
+
+info "Creating executable..."
+chmod +x $LOCAL_PATH/$BINARY
+
+success "Success! Binary has been added to $LOCAL_PATH"
 
