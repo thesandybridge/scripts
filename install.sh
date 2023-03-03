3573cf5b82c76efcf0cb311c4811092d12c1862d
Matt Miller <mattmillerf@pm.me>
2023-03-03 18:05:58 -0500

    add debug and art

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 grey="\\e[37m"
 blue="\\e[36m"
 red="\\e[31m"
@@ -9,6 +11,7 @@ reset="\\e[0m"
 GITHUB_USER=thesandybridge
 GITHUB_REPO=$1
 BINARY=$2
+DEBUG=$3
 LOCAL_PATH=~/.local/bin
 
 cat << "EOM" 
@@ -30,11 +33,25 @@ debug() { if [[ "${DEBUG}" == "true" ]]; then echo -e "${grey}[D] $*${reset}"; f
 success() { echo -e "${green}[✔] $*${reset}"; }
 fail() { echo -e "${red}[✖] $*${reset}"; }
 
+curl_args=
+wget_args=
+enable_debug() {
+    if [[ "${DEBUG}" == "true" ]]; then
+        info "Enabling debug mode."
+        set -x
+        curl_args="-v"
+        wget_args="-v"
+    else
+        curl_args="-s"
+        wget_args="-nv"
+    fi
+}
+enable_debug
+
 if [[ -z $1 ]]; then
     fail "No repository provided"
     exit 1
 fi
-
 
 if [[ -z $2 ]]; then
     fail "No binary name provided"
@@ -42,14 +59,14 @@ if [[ -z $2 ]]; then
 fi
 
 info "Fetching binary from github.com/${GITHUB_USER}/${GITHUB_REPO}..."
-download_linux=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest \
+download_linux=$(curl $curl_args https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest \
 | grep "browser_download_url.*amd64"  \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | xargs)
 
 info "Downloading binary..."
-wget -nv $download_linux -O $LOCAL_PATH/$BINARY
+wget $wget_args $download_linux -O $LOCAL_PATH/$BINARY
 
 info "Creating executable..."
 chmod +x $LOCAL_PATH/$BINARY
