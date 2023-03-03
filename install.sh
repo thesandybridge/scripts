2928be52f9f9734c0b919eb85f7747cc40bd9145
Matt Miller <mattmillerf@pm.me>
2023-03-03 15:13:48 -0500

    change param

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ GITHUB_REPO=$1
 BINARY=cypher
 LOCAL_PATH=~/.local/bin
 
+echo $GITHUB_REPO
+
 download_linux=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest \
 | grep "browser_download_url.*amd64"  \
 | cut -d : -f 2,3 \
