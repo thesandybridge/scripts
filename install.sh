8830a6cf19becd9805c97f7bb79f465881ab36d3
Matt Miller <mattmillerf@pm.me>
2023-03-03 15:18:52 -0500

    add colored output

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+BLUE="34"
+YELLOW="33"
+MAGENTA="35"
+BOLDBLUE="\e[1;${BLUE}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+BOLDMAGENTA="\e[1;${MAGENTA}m"
+ENDCOLOR="\e[0m"
+
 GITHUB_USER=thesandybridge
 GITHUB_REPO=$1
 BINARY=cypher
 LOCAL_PATH=~/.local/bin
 
-echo $GITHUB_REPO
+echo -e "${BOLDBLUE}[+]${ENDCOLOR} Downloading file from github.com/${GITHUB_USER}/${GITHUB_REPO}..."
 
 download_linux=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/latest \
 | grep "browser_download_url.*amd64"  \
