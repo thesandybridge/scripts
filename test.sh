8652c46360cc77c779fba9aa07ddec361ae18620
Matt Miller <mattmillerf@pm.me>
2023-05-04 11:06:11 -0400

    updates

--- test.sh
+++ test.sh
@@ -2,7 +2,10 @@
 
 if [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
     export PATH=${PATH//$HOME\/.local\/bin:/}
-    echo 'export PATH="$PATH"' >> ~/.bashrc
+    if ! grep -q 'export PATH="$PATH"' ~/.bashrc; then
+        echo 'export PATH="$PATH"' >> ~/.bashrc
+        echo 'Added export PATH="$PATH" to ~/.bashrc'
+    fi
     source ~/.bashrc
 fi
 
