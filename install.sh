86d49513f8e80b6cb4d970558e710433ef02a401
Matt Miller <mattmillerf@pm.me>
2025-05-10 13:43:08 -0400

    add common

--- install.sh
+++ install.sh
@@ -12,13 +12,11 @@
 
 set -e
 
-# DEFINE VARIABLES
-grey="\e[37m"
-blue="\e[36m"
-red="\e[31m"
-yellow="\e[33m"
-green="\e[32m"
-reset="\e[0m"
+COMMON_SH="/tmp/common.sh"
+if [ ! -f "$COMMON_SH" ]; then
+    curl -sSL https://scripts.sandybridge.io/common.sh -o "$COMMON_SH"
+fi
+source "$COMMON_SH"
 
 GITHUB_USER=thesandybridge
 GITHUB_REPO=$1
@@ -66,30 +64,6 @@ enable_debug() {
         curl_args="-s"
         wget_args="-q"
     fi
-}
-
-spinner() {
-    # make sure we use non-unicode character type locale
-    # (that way it works for any locale as long as the font supports the characters)
-    local LC_CTYPE=C
-
-    local pid=$1 # Process Id of the previous running command
-    local sp='-\|/'
-    local width=1
-
-    local i=0
-    tput civis # cursor invisible
-    info "$2"
-    while kill -0 $pid 2>/dev/null; do
-        local i=$(((i + $width) % ${#sp}))
-        printf "${blue}%s${reset}" "[${sp:$i:$width}]"
-
-        echo -en "\033[$1D"
-        sleep .1
-    done
-    tput cnorm
-    wait $pid # capture exit code
-    return $?
 }
 
 check_local_dir() {
